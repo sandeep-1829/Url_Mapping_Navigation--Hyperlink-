8d8286ab87d9fab6a83386b7a4c49a75e55726ce
sandeep-1829 <godugunurusandeep@gmail.com>
2023-11-20 23:19:57 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def rohit(request):
+    return render(request,'rohit.html')
+
+def virat(request):
+    return render(request,'virat.html')
